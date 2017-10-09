00df37df1dedd082d02bdbd695bb5d30b9854634
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-10-09 17:27:30 +0800

    add dynamic port.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   res.send("Hello World! Appleboy");
